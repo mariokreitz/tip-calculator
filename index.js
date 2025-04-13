bf8f8b2174b9e40b83fe60dc7c3050b21267812c
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-13 19:00:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,41 @@ document.addEventListener("DOMContentLoaded", () => {
   init();
 });
 const init = () => {
-  const { billElement, buttonElements, peopleElement, timAmountElement, totalAmountElement, resetButton } = getAllDOMElements();
+  const { billElement, buttonElements, customTipElement, peopleElement, tipAmountElement, totalAmountElement, resetButton, errorElement } =
+    getAllDOMElements();
 
   const tipCalculator = new Calculator();
-  tipCalculator.init(billElement, buttonElements, peopleElement, timAmountElement, totalAmountElement, resetButton);
-  tipCalculator.addListeners();
+  tipCalculator.init(
+    billElement,
+    buttonElements,
+    customTipElement,
+    peopleElement,
+    tipAmountElement,
+    totalAmountElement,
+    resetButton,
+    errorElement
+  );
+  tipCalculator.start();
 };
 
 const getAllDOMElements = () => {
   const billElement = document.getElementById("bill");
   const buttonElements = [...document.querySelectorAll(".tip button")];
+  const customTipElement = document.getElementById("custom-tip");
   const peopleElement = document.getElementById("people");
-  const timAmountElement = document.getElementById("tip-amount");
+  const tipAmountElement = document.getElementById("tip-amount");
   const totalAmountElement = document.getElementById("total");
   const resetButton = document.getElementById("reset");
+  const errorElement = document.getElementById("error");
 
   return {
     billElement,
     buttonElements,
+    customTipElement,
     peopleElement,
-    timAmountElement,
+    tipAmountElement,
     totalAmountElement,
     resetButton,
+    errorElement,
   };
 };
